31410ce46e87eb76bef4471d7d13ddb319d4e6ac
swamini05 <91067902+swamini05@users.noreply.github.com>
2024-07-11 23:33:46 +0530

    NumberGuessGame

--- NumberGuessGame.cs
+++ NumberGuessGame.cs
@@ -5,49 +5,48 @@ class NumberGuessGame
     {
         while (true)
         {
-            int randno = Newnum(1, 11);
-            Console.WriteLine(randno);
-            int count = 1;
+            int randomNumber = NewNum(1, 11);
+            int counter = 1;
 
-            while (count <= 3)
+            while (counter <= 3)
             {
                 Console.Write("Enter a number between 1 and 10 (0 to quit): ");
-                int input = Convert.ToInt32(Console.ReadLine());
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
 
-                if (input < 0 || input > 10)
+                if (inputNumber < 0 || inputNumber > 10)
                 {
                     Console.WriteLine("Invalid input, please enter a valid number.");
                     continue;
                 }
                 
-                if (input == 0)
+                if (inputNumber == 0)
                     return;
-                if (input < randno && count < 3)
+                if (inputNumber < randomNumber && counter < 3)
                 {
                     Console.WriteLine("Too Low, Try Again.");
-                    ++count;
+                    ++counter;
                     continue;
                 }
-                else if (input > randno && count < 3)
+                else if (inputNumber > randomNumber && counter < 3)
                 {
                     Console.WriteLine("Too High, Try Again.");
-                    ++count; 
+                    ++counter; 
                     continue;
                 }
-                else if(input == randno)
+                else if(inputNumber == randomNumber)
                 {
-                    Console.WriteLine(count < 3 ? "Congratulation! You guessed it in {0}{1} attempt.\n" : "Congratulation! You won the game.\n", count, count == 1 ? "st": "nd");
+                    Console.WriteLine(counter < 3 ? "Congratulation! You guessed it in {0}{1} attempt.\n" : "Congratulation! You won the game.\n", counter, counter == 1 ? "st": "nd");
                     break;
                 }
                 else
                 {
-                   Console.WriteLine("Sorry you lost the Game! The number was "+ randno +".\n"); 
+                   Console.WriteLine("Sorry you lost the Game! The number was "+ randomNumber +".\n"); 
                    break;
                 }
             }
         }
     }
-    static int Newnum(int min, int max)
+    static int NewNum(int min, int max)
     {
         Random random = new Random();
         return random.Next(min, max);
