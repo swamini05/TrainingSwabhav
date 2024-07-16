12a41aecc8436a72a9f576850635a38745c4af72
swamini05 <91067902+swamini05@users.noreply.github.com>
2024-07-16 21:54:47 +0530

    EnumDemoFolder

---
+++ EnumDemo/Grade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    enum Grade
+    {
+
+        A=10,
+        B,
+        C=30,
+        D,
+    }
+
+    
+    
+}
---
+++ EnumDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grade myVar = Grade.B;
+            switch (myVar)
+            {
+                case Grade.A:
+                    Console.WriteLine("Great!");
+                    break;
+                case Grade.B:
+                    Console.WriteLine("Keep it up!");
+                    break;
+                case Grade.C:
+                    Console.WriteLine("Work Harder!");
+                    break;
+            }
+            Console.WriteLine((int)myVar);
+        }
+    }
+}
