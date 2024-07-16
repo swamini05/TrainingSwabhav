47cddcceb38f1f3b4abda81a839463558d89a88a
swamini05 <91067902+swamini05@users.noreply.github.com>
2024-07-16 12:19:52 +0530

    PlayerApp

---
+++ PlayerApp/Models/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Models
+{
+    internal class Player
+    {
+        static int DEFAULT_AGE = 18;
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; } = DEFAULT_AGE;
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            
+        }
+        public Player(int id,string name,int age):this(id,name) 
+        {
+            Age = age;
+            
+        }
+        public static Player WhoIsElder(Player[] players)
+        {
+            Player elderPlayer = players[0];
+            foreach (Player player in players)
+            {
+                if (player.Age > elderPlayer.Age)
+                {
+                    elderPlayer = player;
+                }
+            }
+            return elderPlayer;
+        }
+    }
+}
---
+++ PlayerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Principal;
+using PlayerApp.Models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[]
+            {
+                new Player (101, "Allen"),
+                new Player(102, "Mary", 22),
+                new Player(102,"Swamini",21),
+                new Player(104,"Swati",24),
+                new Player(105,"abc",28)
+            };
+            Player elderPlayer = Player.WhoIsElder(players);
+            Console.WriteLine($"Elder Player: ID = {elderPlayer.Id}, Name = {elderPlayer.Name}, Age = {elderPlayer.Age}");
+        }
+
+    }
+}
+
