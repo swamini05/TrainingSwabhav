bf46ffdb68bce0a3ab605067a9240fcb33f79a0b
swamini05 <91067902+swamini05@users.noreply.github.com>
2024-07-22 21:58:19 +0530

    FileHandling

---
+++ FileHandlingDemo/Models/ReadWrite.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingDemo.Models
+{
+    internal class ReadWrite
+    {
+        static string filePath = "D:\\Training\\FileHandlingDemo\\";
+        public static void WriteToFile()
+        {
+            Console.WriteLine("Enter the file name to write to:");
+            string fileName = Console.ReadLine();
+
+            Console.WriteLine("Enter the content to write:");
+            string content = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filePath+fileName))
+            {
+                writer.Write(content);
+            }
+            Console.WriteLine($"Content written to {fileName}.");
+        }
+        public static void ReadFromFile()
+        {
+            Console.WriteLine("Enter the file path to read from:");
+            string fileName = Console.ReadLine();
+            if (!File.Exists(filePath+fileName))
+            {
+                Console.WriteLine("create a file first then read it!");
+                return;
+            }
+            using (StreamReader reader = new StreamReader(filePath+fileName))
+            {
+                Console.WriteLine("File content:");
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        }
+        public static void AddUserName()
+        {
+            Console.WriteLine("Enter your username: ");
+            string username = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filePath+"Usernametest.txt", append: true))
+            {
+                writer.WriteLine(username);
+            }
+            Console.WriteLine("Username has been written to the file.");
+        }
+    }
+}
---
+++ FileHandlingDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using FileHandlingDemo.Models;
+
+namespace FileHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter:\n1. To write to a file \n" +
+                    "2. To read from a file\n" +
+                    "3. To add username to the file\n" +
+                    "4. Exit");
+                int action = Convert.ToInt32(Console.ReadLine());
+
+                switch (action)
+                {
+                    case 1:
+                        ReadWrite.WriteToFile();
+                        break;
+                    case 2:
+                        ReadWrite.ReadFromFile();
+                        break;
+                    case 3:
+                        ReadWrite.AddUserName();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid action, please try again.\n");
+                        break;
+                }
+            }
+        }
+    }
+}
